455d617316f6a14349bfa75cae151797c1d6d7e6
Farquhar13 <farquharc356@strose.edu>
2018-03-24 00:38:20 -0400

    eightpuzzle is the most current
    latest tested methods:
    constructor
    printPuzzle
    init_goal_state
    get_lowest_f
    update_f
    win_condition
    
    To do:
    looping method
    Test/rewrite expansion method

--- eightpuzzle.py
+++ eightpuzzle.py
@@ -1,23 +1,163 @@
 import sys
+import copy
 
 boardList = []
+goal_state = [['0','1','2'], ['3','4','5'], ['6','7','8']]
 
 class Board:
-    def __init__(self):
-        self.state = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-        self.f = -1  # function = g + h
+    def __init__(self, argv_starting_index):
+        self.state = self.setState(argv_starting_index)
+        self.h = self.manhattanDistance()
         self.g = 0  # cost function
-        self.h = -1  # hueristic = Manhattan distance
+        self.f = self.update_f()
         self.moveList = []
-        self.expanded = False
 
-    # Initializes the board state
-    def setState(self, argvStartIndex):
-        index = argvStartIndex
-        for i in range(0, 3):  # loops through rows
-            for j in range(0, 3):  # loops through columns
-                self.state[i][j] = sys.argv[index]
+    # Initializes the starting board state
+    def setState(self, argv_starting_index):
+        index = argv_starting_index  # 1 is the index for starting state
+        state = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+        for i in range(3):  # loops through rows
+            for j in range(3):  # loops through columns
+                state[i][j] = sys.argv[index]
                 index = index + 1
+        return state
+
+    # Calculates h and calls method to update f
+    def manhattanDistance(self):
+        totalDistance = 0
+        for i in range(3):
+            for j in range(3):
+                block = self.state[i][j]  # Goes through block by block
+                coordinate = self.goalCoordinates(block)  # finds the coordinate of the same block in the goal state
+                blockDistance = abs(i - coordinate[0]) + abs(j - coordinate[1])  # determines how far the block is from the goal state position
+                totalDistance += blockDistance  # sums each block's distance from goal position
+        return totalDistance
+
+    # Finds the coordinate of a block, given as an argument
+    def goalCoordinates(self, block):
+        for i in range(0,3):
+            for j in range(0,3):
+                if block == goal_state[i][j]:
+                    coordinate = i, j
+                    return coordinate
+
+    def find_coordinate(self, block):
+        for i in range(0,3):
+            for j in range(0,3):
+                if block == self.state[i][j]:
+                    coordinate = i, j
+                    return coordinate
+
+
+    # Updates our function f
+    def update_f(self):
+        self.win_condition()
+        return self.h + self.g
+
+    def win_condition(self):
+        if self.h == 0:
+            print "Manhattan distance = 0!"
+            for m in self.moveList:
+                print m
+            sys.exit(0)
+
+    # Expands new possible states
+    def expand(self):
+        global boardList
+        zeroX, zeroY = self.find_coordinate('0')
+
+        # Checks if a block can be moved up
+        if 0 <= zeroX + 1 <=2:
+            #print "up"
+            boardList.append(copy.deepcopy(self))
+
+            block = boardList[-1].state[zeroX + 1][zeroY]
+            boardList[-1].state[zeroX + 1][zeroY] = '0'
+            boardList[-1].state[zeroX][zeroY] = block
+            #print boardList[-1].state
+
+            if boardList[-1].uniqueState():
+                boardList[-1].moveList.append([block, "up"])
+                boardList[-1].g = boardList[-1].g + 1
+                boardList[-1].manhattanDistance()
+                boardList[-1].expanded = False
+            else:
+                #print "delete 1"
+                del boardList[-1]
+
+        # Checks if a block can be moved down
+        if 0 <= zeroX - 1 <=2:
+            #print "down"
+            boardList.append(copy.deepcopy(self))
+
+            block = boardList[-1].state[zeroX - 1][zeroY]
+            boardList[-1].state[zeroX - 1][zeroY] = '0'
+            boardList[-1].state[zeroX][zeroY] = block
+            #print boardList[-1].state
+
+            if boardList[-1].uniqueState():
+                boardList[-1].moveList.append([block, "down"])
+                boardList[-1].g = boardList[-1].g + 1
+                boardList[-1].manhattanDistance()
+                boardList[-1].expanded = False
+            else:
+                #print "delete 2"
+                del boardList[-1]
+
+        # Checks if a block can be moved left
+        if 0 <= zeroY + 1 <=2:
+            #print "left"
+            boardList.append(copy.deepcopy(self))
+
+            block = boardList[-1].state[zeroX][zeroY + 1]
+            boardList[-1].state[zeroX][zeroY + 1] = '0'
+            boardList[-1].state[zeroX][zeroY] = block
+            #print boardList[-1].state
+
+            if boardList[-1].uniqueState():
+                boardList[-1].moveList.append([block, "left"])
+                boardList[-1].g = boardList[-1].g + 1
+                boardList[-1].manhattanDistance()
+                boardList[-1].expanded = False
+            else:
+                #print "delete 3"
+                #print boardList[-1].state
+                #print "len before", len(boardList)
+                del boardList[-1]
+                #print "len after", len(boardList)
+
+
+        # Checks if a block can be moved right
+        if 0 <= zeroY - 1 <=2:
+            #print "right"
+            boardList.append(copy.deepcopy(self))
+
+            block = boardList[-1].state[zeroX][zeroY - 1]
+            boardList[-1].state[zeroX][zeroY - 1] = '0'
+            boardList[-1].state[zeroX][zeroY] = block
+            #print boardList[-1].state
+
+            if boardList[-1].uniqueState():
+                boardList[-1].moveList.append([block, "right"])
+                boardList[-1].g = boardList[-1].g + 1
+                boardList[-1].manhattanDistance()
+                boardList[-1].expanded = False
+            else:
+                #print "delete 4"
+                #print boardList[-1].state
+                #print "len before", len(boardList)
+                del boardList[-1]
+                #print "len after", len(boardList)
+
+    # Returns the board with the lowest f and removes it from boardList
+    def get_lowest_f(self):
+            minimum = sys.maxint
+            min_index = 0
+            for i in range(len(boardList)):
+                if boardList[i].f < minimum:
+                    minimum = boardList[i].f
+                    min_index = i
+            return boardList.pop(min_index)
 
     # Prints the state of a puzzle
     def printPuzzle(self):
@@ -26,158 +166,23 @@ class Board:
                 print block,
             print "\n"
 
-    # Calculates h and calls method to update f
-    def manhattanDistance(self):
-        totalDistance = 0
-        for i in range(0,3):
-            for j in range(0,3):
-                block = self.state[i][j]  # Goes through block by block
-                coordinate = goalState.goalCoordinates(block)  # finds the coordinate of the same block in the goal state
-                blockDistance = abs(i - coordinate[0]) + abs(j - coordinate[1])  # determines how far the block is from the goal state position
-                totalDistance = totalDistance + blockDistance  # sums each block's distance from goal position
-        self.h = totalDistance
-        if self.h == 0:
-            pass  # win condition
-        self.updateF()
 
-    # Finds the coordinate of a block, given as an argument
-    def goalCoordinates(self, block):
-        for i in range(0,3):
-            for j in range(0,3):
-                if block == self.state[i][j]:
-                    coordinate = i, j
-                    return coordinate
+def init_goal_state(argv_index):
+    for row in range(3):
+        for column in range(3):
+            goal_state[row][column] = sys.argv[argv_index]
+            argv_index = argv_index + 1
 
-    # Updates our function f
-    def updateF(self):
-        self.f = self.h + self.g
+init_goal_state(10)
+start_state = Board(1)
 
-    # Expands new possible states
-    def expand(self):
-        zeroX, zeroY = self.goalCoordinates('0')
-        print "zeroX:", zeroX
-        print "zeroY:", zeroY
-
-        # Checks if a block can be moved up
-        if 0 <= zeroX + 1 <=2:
-            Successor(self, "up")
-
-        '''# Checks if a block can be moved down
-        if 0 <= zeroX - 1 <=2:
-            newState = self
-            block = self.state[zeroX - 1][zeroY]
-            newState.state[zeroX][zeroY] = block
-            newState.state[zeroX - 1][zeroY] = '0'
-
-            if self.uniqueState:
-                newState.moveList.append([block, "down"])
-                boardList.append(newState)
-                newState.g = self.g + 1
-                self.manhattanDistance()
-
-        # Checks if a block can be moved left
-        if 0 <= zeroY + 1 <=2:
-            newState = self
-            block = self.state[zeroX][zeroY + 1]
-            newState.state[zeroX][zeroY] = block
-            newState.state[zeroX][zeroY + 1] = '0'
-
-            if self.uniqueState:
-                newState.moveList.append([block, "left"])
-                boardList.append(newState)
-                newState.g = self.g + 1
-                self.manhattanDistance()
-
-        # Checks if a block can be moved right
-        if 0 <= zeroY - 1 <=2:
-            newState = self
-            block = self.state[zeroX][zeroY - 1]
-            newState.state[zeroX][zeroY] = block
-            newState.state[zeroX][zeroY - 1] = '0'
-
-            if self.uniqueState:
-                newState.moveList.append([block, "right"])
-                boardList.append(newState)
-                newState.g = self.g + 1
-                self.manhattanDistance()'''
-
-
-    # Checks if an expanded state is already in boardList
-    def uniqueState(self):
-        for board in boardList:
-            if self.state == board.state:
-                return False
-            else:
-                return True
-
-    # Finds the board with the lowest f that hasn't been expanded
-    def lowestF(self):
-        min = None  # very high initialization value
-        for i in range(len(boardList)):
-            if boardList[i].expanded == False and min == None:
-                min = boardList[i].f
-                minIndex = i
-            if boardList[i].expanded == False and boardList[i].f < min:
-                min = boardList[i].f
-                minIndex = i
-
-        boardList[minIndex].expand()  # selects lowest board with lowest f to expand in moveBlock
-
-
-class Successor(Board):
-    def __init__(self, parent, direction):
-        global boardList
-
-        self.state = parent.state
-        self.f = parent.f  # function = g + h
-        self.g = parent.g  # cost function
-        self.h = parent.h  # hueristic = Manhattan distance
-        self.moveList = parent.moveList
-        self.expanded = False
-
-        if direction == "up":
-            print "pre parent.state", parent.state
-            zeroX, zeroY = self.goalCoordinates('0')
-            block = self.state[zeroX + 1][zeroY]
-            self.state[zeroX + 1][zeroY] = '0'
-            self.state[zeroX][zeroY] = block
-            print "post parent.state:", parent.state  # BUG! changes parent.state
-            if self.uniqueState():
-                print "check 2"  # BUG! does not print
-                self.moveList.append(block, "up")
-                self.g = self.g + 1
-                self.manhattanDistance()
-                boardList.append(self)
-
-
-
-startState = Board()
-startState.setState(1)
-boardList.append(startState)
+boardList.append(start_state)
 print "Start State:"
-startState.printPuzzle()
+start_state.printPuzzle()
 
-goalState = Board()
-goalState.setState(10)
-print "Goal State:"
-goalState.printPuzzle()
-
-startState.manhattanDistance()
-print "Starting Manhattan distance:", startState.h
-
-print "Function total:", startState.f
-
-print
-print "boardList before:"
+'''print "boardList after:"
 for b in boardList:
-    print b.state
-
-startState.lowestF()
-
-print
-print "boardList after:"
-for b in boardList:
-    print b.state
+    print b.state'''
 
 
 
--- eightpuzzle4.py
+++ eightpuzzle4.py
@@ -2,68 +2,71 @@ import sys
 import copy
 
 boardList = []
+goal_state = [['0','1','2'], ['3','4','5'], ['6','7','8']]
 
 class Board:
-    def __init__(self):
-        self.state = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-        self.f = -1  # function = g + h
+    def __init__(self, argv_starting_index):
+        self.state = self.setState(argv_starting_index)
+        self.h = self.manhattanDistance()
         self.g = 0  # cost function
-        self.h = -1  # hueristic = Manhattan distance
+        self.f = self.updateF()
         self.moveList = []
-        self.expanded = False
 
-    # Initializes the board state
-    def setState(self, argvStartIndex):
-        index = argvStartIndex
-        for i in range(0, 3):  # loops through rows
-            for j in range(0, 3):  # loops through columns
-                self.state[i][j] = sys.argv[index]
+    # Initializes the starting board state
+    def setState(self, argv_starting_index):
+        index = argv_starting_index  # 1 is the index for starting state
+        state = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+        for i in range(3):  # loops through rows
+            for j in range(3):  # loops through columns
+                state[i][j] = sys.argv[index]
                 index = index + 1
-
-    # Prints the state of a puzzle
-    def printPuzzle(self):
-        for row in self.state:
-            for block in row:
-                print block,
-            print "\n"
+        return state
 
     # Calculates h and calls method to update f
     def manhattanDistance(self):
         totalDistance = 0
-        for i in range(0,3):
-            for j in range(0,3):
+        for i in range(3):
+            for j in range(3):
                 block = self.state[i][j]  # Goes through block by block
-                coordinate = goalState.goalCoordinates(block)  # finds the coordinate of the same block in the goal state
+                coordinate = self.goalCoordinates(block)  # finds the coordinate of the same block in the goal state
                 blockDistance = abs(i - coordinate[0]) + abs(j - coordinate[1])  # determines how far the block is from the goal state position
                 totalDistance = totalDistance + blockDistance  # sums each block's distance from goal position
-        self.h = totalDistance
-        #print self.h
-        #print
-
-        # Win condition
-        if self.h == 0:
-            print "Manhattan distance = 0!"
-            for m in self.moveList:
-                print m
-                sys.exit()
-        self.updateF()
+        return totalDistance
 
     # Finds the coordinate of a block, given as an argument
     def goalCoordinates(self, block):
+        for i in range(0,3):
+            for j in range(0,3):
+                if block == goal_state[i][j]:
+                    coordinate = i, j
+                    return coordinate
+
+    def find_coordinate(self, block):
         for i in range(0,3):
             for j in range(0,3):
                 if block == self.state[i][j]:
                     coordinate = i, j
                     return coordinate
 
+
     # Updates our function f
     def updateF(self):
-        self.f = self.h + self.g
+        return self.h + self.g
+
+    def win_condition(self):
+        # Win condition
+        if self.h == 0:
+            print
+            "Manhattan distance = 0!"
+            for m in self.moveList:
+                print
+                m
+                sys.exit()
 
     # Expands new possible states
     def expand(self):
         global boardList
-        zeroX, zeroY = self.goalCoordinates('0')
+        zeroX, zeroY = self.find_coordinate('0')
 
         # Checks if a block can be moved up
         if 0 <= zeroX + 1 <=2:
@@ -148,76 +151,40 @@ class Board:
                 del boardList[-1]
                 #print "len after", len(boardList)
 
-
-    # Checks if an expanded state is already in boardList
-    def uniqueState(self):
-        global boardList
-        for board in boardList[:-1]:
-            if self.state == board.state:
-                #print "False"
-                return False
-            else:
-                #print "True"
-                return True
-
-    # Finds the board with the lowest f that hasn't been expanded
-    def lowestF(self):
-        count = 0
-        while True:
-            #this section for testing purposes
-            '''count = count + 1
-            if count == 5:
-                print "5 iterations"
-                if boardList[-1].state == boardList[6].state:
-                    print "equals"
-                print "boardList after:"
-                for b in boardList:
-                    print b.state
-                sys.exit()'''
-
-            min = sys.maxint
-            minBoard = None
-
-            '''for board in boardList:
-                if board.expanded == False and board.f < min:
-                    min = board.f
-                    minBoard = board
-
-            minBoard.expanded = True
-            minBoard.expand()  # selects lowest board with lowest Manhattan distance to expand'''
-            minIndex = 0
+    # Returns the board with the lowest f and removes it from boardList
+    def get_lowest_f(self):
+            minimum = sys.maxint
+            min_index = 0
             for i in range(len(boardList)):
-                if boardList[i].expanded == False and boardList[i].f < min:
-                    min = boardList[i].f
-                    minIndex = i
-                    #print "minIndex", minIndex
-            #print "minIndex:", minIndex
-            boardList[minIndex].expanded = True
-            boardList[minIndex].expand()  # selects lowest board with lowest f to expand in moveBlock
+                boardList[i].win_condition
+                if boardList[i].f < minimum:
+                    minimum = boardList[i].f
+                    min_index = i
+            return boardList.pop(min_index)
+
+    # Prints the state of a puzzle
+    def printPuzzle(self):
+        for row in self.state:
+            for block in row:
+                print block,
+            print "\n"
 
 
+def init_goal_state(argv_index):
+    for row in range(3):
+        for column in range(3):
+            goal_state[row][column] = sys.argv[argv_index]
+            argv_index = argv_index + 1
 
+init_goal_state(10)
+start_state = Board(1)
 
-startState = Board()
-startState.setState(1)
-boardList.append(startState)
+boardList.append(start_state)
 print "Start State:"
-startState.printPuzzle()
+start_state.printPuzzle()
 
-goalState = Board()
-goalState.setState(10)
-print "Goal State:"
-goalState.printPuzzle()
-
-print
-
-#goalState.manhattanDistance()
-
-print "boardList before:"
-for b in boardList:
-    print b.state
-
-startState.lowestF()
+x = start_state.get_lowest_f()
+print x.state
 
 '''print "boardList after:"
 for b in boardList:
